58b6899da15d426e94e1f90ec957881ae4f4eee3
Sergey Boykov <sutemi67@gmail.com>
2025-08-21 13:47:37 +0300

    patch

--- composeApp/src/commonMain/kotlin/apc/appcradle/kotlinkmp_first_app/App.kt
+++ composeApp/src/commonMain/kotlin/apc/appcradle/kotlinkmp_first_app/App.kt
@@ -3,20 +3,25 @@ package apc.appcradle.kotlinkmp_first_app
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import apc.appcradle.kotlinkmp_first_app.components.AppOutlinedField
 import kotlinkmp_first_app.composeapp.generated.resources.Res
+import kotlinkmp_first_app.composeapp.generated.resources.grads_value
 import kotlinkmp_first_app.composeapp.generated.resources.minutes
+import kotlinkmp_first_app.composeapp.generated.resources.radians_value
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -24,8 +29,18 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        var degrees by remember { mutableStateOf(0.0) }
         val calc = Calculator()
+        var degrees by remember { mutableStateOf(0.0) }
+        var grads by remember { mutableStateOf(0.0) }
+        var radians by remember { mutableStateOf(0.0) }
+        var minutes by remember { mutableStateOf(Triple(0, 0, 0)) }
+
+        LaunchedEffect(degrees) {
+            grads = calc.getGrads(degrees)
+            radians = calc.getRadians(degrees)
+            minutes = calc.getMinutes(degrees)
+        }
+        
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -34,16 +49,16 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text("Degrees calculator")
-            HorizontalDivider()
+            HorizontalDivider(Modifier.padding(20.dp))
             AppOutlinedField { degrees = it }
-            Text(text = "Grads: ${calc.getGrads(degrees)}")
-            Text(text = "Radians: ${calc.getRadians(degrees)}")
+            Text(text = stringResource(Res.string.grads_value, grads))
+            Text(text = stringResource(Res.string.radians_value, radians))
             Text(
                 text = stringResource(
                     Res.string.minutes,
-                    calc.getMinutes(degrees).first,
-                    calc.getMinutes(degrees).second,
-                    calc.getMinutes(degrees).third
+                    minutes.first,
+                    minutes.second,
+                    minutes.third
                 )
             )
         }
